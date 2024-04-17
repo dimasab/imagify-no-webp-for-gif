870f7d7f2ca5eade779cbbbf7295f4ee81ca045c
Dimas <78547184+dimasab@users.noreply.github.com>
2024-04-17 21:18:12 +0700

    Add files via upload

---
+++ imagify-no-webp-for-gif.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Imagify - No WEBP for GIF
+ * Description: Excludes GIF files from being converted into WEBP
+ * Plugin URI:  https://github.com/dimasab/imagify-no-webp-for-gif/
+ * Author:      Dimas Adika Bimawan
+ */
+
+namespace Imagify\my_custom_plugin;
+
+// Standard plugin security, keep this line in place.
+defined( 'ABSPATH' ) || die();
+
+/**
+ * Stop gifs from being converted into webp
+ */
+
+function no_webp_for_gif( $response, $process, $file, $thumb_size, $optimization_level, $webp, $is_disabled ) {
+	if ( ! $webp || $is_disabled || is_wp_error( $response ) ) {
+		return $response;
+	}
+
+	if ( 'image/gif' !== $file->get_mime_type() ) {
+		return $response;
+	}
+
+	return new \WP_Error( 'no_webp_for_gif', __( 'Webp version of gif is disabled by filter.' ) );
+}
+
+add_filter( 'imagify_before_optimize_size', __NAMESPACE__ . '\no_webp_for_gif' , 9, 7 );
